b4ffd8d1a4159b2b947a2188934afded8a3192ef
Dofamin71 <mitya2la@gmail.com>
2020-09-18 23:58:01 +0300

    The Second Lesson

---
+++ src/main/java/ru/dofamin/spring/Music.java
@@ -0,0 +1,5 @@
+package ru.dofamin.spring;
+
+public interface Music {
+    String getSong();
+}
---
+++ src/main/java/ru/dofamin/spring/RockMusic.java
@@ -0,0 +1,8 @@
+package ru.dofamin.spring;
+
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Wind cries Mary";
+    }
+}
